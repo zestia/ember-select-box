119d1fd8f02ed4b9be870b8c8908432180d6675e
Andrew Kirwin <andrew.kirwin@gmail.com>
2017-05-19 10:49:49 +0100

    Added ember-cli-github-pages addon

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -39,5 +39,10 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
+  if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/ember-select-box/';
+  }
+
   return ENV;
 };
