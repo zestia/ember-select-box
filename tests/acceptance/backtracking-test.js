dc23e7edbe13f91bf01d222ca9865f02d67fc893
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-03-01 08:58:34 +0000

    remove backtrack select files, as they might confuse new users when we link to that folder

--- tests/acceptance/backtracking-test.js
+++ tests/acceptance/backtracking-test.js
@@ -1,45 +1,71 @@
 import Ember from 'ember';
-import { test, skip } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { module, skip, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { visit, click, findAll, find, triggerEvent, settled } from '@ember/test-helpers';
+import Component from '@ember/component';
+import hbs from 'htmlbars-inline-precompile';
 
 const runTest = parseFloat(Ember.VERSION) === 2.8 ? skip : test;
 
-moduleForAcceptance('backtracking focus use-case');
+module('backtracking focus use-case', function(hooks) {
+  setupApplicationTest(hooks);
 
-// Try scenario in a Twiddle:
-// https://ember-twiddle.com/2f066b448da4242c29e241a5203d8840
+  hooks.beforeEach(function() {
+    this.owner.register('template:backtrack-select', hbs`
+      {{#backtrack-select as |bs|}}
+        {{#bs.selected-option}}
+          open
+        {{/bs.selected-option}}
+        {{#bs.options}}
+          <button onclick={{action bs.close}} class="backtrack-select-close">close</button>
+        {{/bs.options}}
+      {{/backtrack-select}}
+    `);
 
-runTest('backtracking focus error', function(assert) {
-  assert.expect(6);
+    this.owner.register('component:backtrack-select', Component.extend({
+      layout: hbs`
+        {{#select-box class-prefix="backtrack-select" as |sb|}}
+          {{yield (hash
+            selected-option=(component sb.selected-option click=sb.open)
+            close=sb.close
+            options=(if sb.isOpen sb.options)
+          )}}
+        {{/select-box}}
+      `
+    }));
+  });
 
-  visit('/backtrack-select');
+  // Try scenario in a Twiddle:
+  // https://ember-twiddle.com/2f066b448da4242c29e241a5203d8840
 
-  andThen(() => {
-    assert.equal(find('.backtrack-select-options').length, 0,
+  runTest('backtracking focus error', async function(assert) {
+    assert.expect(6);
+
+    await visit('/backtrack-select');
+
+    assert.equal(findAll('.backtrack-select-options').length, 0,
       'precondition: options not rendered when closed');
 
-    assert.ok(!find('.backtrack-select').hasClass('is-focused'),
+    assert.ok(!find('.backtrack-select').classList.contains('is-focused'),
       'precondition: not focused');
-  });
 
-  triggerEvent('.backtrack-select', 'focus');
-  click('.backtrack-select-selected-option:contains("open")');
+    await triggerEvent('.backtrack-select', 'focus');
+    await click('.backtrack-select-selected-option');
 
-  andThen(() => {
-    assert.equal(find('.backtrack-select-options').length, 1,
+    assert.equal(findAll('.backtrack-select-options').length, 1,
       'precondition: options rendered when open');
 
-    assert.ok(find('.backtrack-select').hasClass('is-focused'),
+    assert.ok(find('.backtrack-select').classList.contains('is-focused'),
       'precondition: focused');
-  });
 
-  click('.backtrack-select button:contains("close")');
+    await click('.backtrack-select-close');
 
-  andThen(() => {
-    assert.equal(find('.backtrack-select-options').length, 0,
+    assert.equal(findAll('.backtrack-select-options').length, 0,
       'options destroyed successfully without throwing double render error');
 
-    assert.ok(!find('.backtrack-select').hasClass('is-focused'),
+    await settled();
+
+    assert.ok(!find('.backtrack-select').classList.contains('is-focused'),
       'no longer focused');
   });
 });
