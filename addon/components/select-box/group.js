3f9297cfb9983694a5fb741d6242a4b854903632
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-04-18 19:10:52 +0100

    move to template only component

--- addon/components/select-box/group.js
+++ addon/components/select-box/group.js
@@ -1,10 +1,7 @@
 import Component from '@ember/component';
-import Nameable from '../../mixins/general/nameable';
 import layout from '../../templates/components/select-box/group';
 
-const mixins = [Nameable];
-
-export default Component.extend(...mixins, {
+export default Component.extend({
   layout,
-  classNameSuffix: 'group'
+  tagName: ''
 });
