4c372abe8817aac23d59684381acd85482da8326
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-05-24 14:53:16 +0100

    remove unused imports

--- tests/integration/components/option-test.js
+++ tests/integration/components/option-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, settled, find, findAll } from '@ember/test-helpers';
+import { render, settled, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { defer } from 'rsvp';
 const { from } = Array;
