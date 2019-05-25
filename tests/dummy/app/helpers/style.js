043e7efbfa4c388b7c65399d85ef759681430468
Andrew Kirwin <andrew.kirwin@gmail.com>
2019-05-25 19:38:32 +0100

    lint template

---
+++ tests/dummy/app/helpers/style.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import { helper } from '@ember/component/helper';
+import { htmlSafe } from '@ember/string';
+const { escapeExpression } = Ember.Handlebars.Utils;
+
+export default helper(function(args) {
+  return htmlSafe(escapeExpression(...args));
+});
