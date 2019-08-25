5036fb4f5241f2d0e6e84688de601b4bbfafd794
Andrew Kirwin <andrew.kirwin@gmail.com>
2019-08-25 20:53:16 +0100

    begin removing mixins

--- addon/components/select-box.js
+++ addon/components/select-box.js
@@ -1,6 +1,4 @@
 import Component from '@ember/component';
-import ActivatableOptions from '../mixins/select-box/activatable-options';
-import ActivatableSelectedOptions from '../mixins/select-box/activatable-selected-options';
 import BaseSelectBox from '../mixins/select-box/base';
 import BuildSelection from '../mixins/select-box/build-selection';
 import ClickOutsideEvent from '../mixins/select-box/click-outside-event';
@@ -21,10 +19,14 @@ import SelectActiveOptionOnEnter from '../mixins/select-box/select-active-option
 import API from '../mixins/select-box/api';
 import Toggleable from '../mixins/select-box/toggleable';
 import { or } from '@ember/object/computed';
+import { computed, get, set } from '@ember/object';
+import scrollIntoView from '../utils/select-box/scroll-into-view';
+import escapeRegExp from '../utils/escape-regexp';
+import collapseWhitespace from '../utils/collapse-whitespace';
+const { fromCharCode } = String;
+export const COLLECT_CHARS_MS = 1000;
 
 const mixins = [
-  ActivatableOptions,
-  ActivatableSelectedOptions,
   API,
   BaseSelectBox,
   BuildSelection,
@@ -49,5 +51,182 @@ export default Component.extend(...mixins, {
   layout,
   tagName: '',
 
-  isBusy: or('isPending', 'isSearching')
+  isBusy: or('isPending', 'isSearching'),
+
+  activeSelectedOption: computed(
+    'activeSelectedOptionIndex',
+    'selectedOptions',
+    function() {
+      return this.selectedOptions.objectAt(
+        get(this, 'activeSelectedOptionIndex')
+      );
+    }),
+
+  activeOption: computed('activeOptionIndex', 'options', function() {
+    return this.options.objectAt(get(this, 'activeOptionIndex'));
+  }),
+
+  init() {
+    this._super(...arguments);
+    this._deactivateOptions();
+    this._deactivateSelectedOptions();
+  },
+
+  actions: {
+    activateOptionAtIndex(index, scroll = false) {
+      this._activateOptionAtIndex(index, scroll);
+    },
+
+    activateSelectedOptionAtIndex(index, scroll = false) {
+      this._activateSelectedOptionAtIndex(index, scroll);
+    },
+
+    activateNextOption(scroll = true) {
+      const next = this.activeOptionIndex + 1;
+      this._activateOptionAtIndex(next, scroll);
+    },
+
+    activateNextSelectedOption(scroll = true) {
+      const next = this.activeSelectedOptionIndex + 1;
+      this._activateSelectedOptionAtIndex(next, scroll);
+    },
+
+    activatePreviousOption(scroll = true) {
+      const prev = this.activeOptionIndex - 1;
+      this._activateOptionAtIndex(prev, scroll);
+    },
+
+    activatePreviousSelectedOption(scroll = true) {
+      const prev = this.activeSelectedOptionIndex - 1;
+      this._activateSelectedOptionAtIndex(prev, scroll);
+    },
+
+    activateOptionForKeyCode(keyCode, scroll = true) {
+      const char = fromCharCode(keyCode);
+
+      if (char) {
+        this._activateOptionForChar(char, scroll);
+      }
+    },
+
+    deactivateOptions() {
+      this._deactivateOptions();
+    },
+
+    deactivateSelectedOptions() {
+      this._deactivateSelectedOptions();
+    }
+  },
+
+  _activateOptionAtIndex(index, scroll) {
+    const under = index < 0;
+    const over = index > this.options.length - 1;
+
+    if (!(under || over)) {
+      set(this, 'activeOptionIndex', index);
+      this._activatedOption();
+    }
+
+    if (scroll) {
+      this._scrollActiveOptionIntoView();
+    }
+  },
+
+  _activateSelectedOptionAtIndex(index, scroll) {
+    const under = index < 0;
+    const over = index > this.selectedOptions.length - 1;
+
+    if (!(under || over)) {
+      set(this, 'activeSelectedOptionIndex', index);
+      this._activatedSelectedOption();
+    }
+
+    if (scroll) {
+      this._scrollActiveSelectedOptionIntoView();
+    }
+  },
+
+  _activateOptionForChar(char, scroll) {
+    const lastChars = this._activateOptionChars || '';
+    const lastMs = this._activateOptionMs || 0;
+    const lastIndex = this._activateOptionIndex || 0;
+    const lastChar = lastChars.substring(lastChars.length - 1);
+    const ms = Date.now();
+    const duration = ms - lastMs;
+    const repeatedChar = char === lastChar;
+    const reset = duration > COLLECT_CHARS_MS;
+    const chars = reset ? char : `${lastChars}${char}`;
+    let options = this._findOptionsMatchingChars(chars);
+    let index = 0;
+    let option;
+
+    if (repeatedChar) {
+      index = lastIndex + 1;
+      options = this._findOptionsMatchingChars(lastChar);
+      option = options[index];
+    }
+
+    if (!option) {
+      index = 0;
+      option = options[index];
+    }
+
+    if (option) {
+      this.send('activateOptionAtIndex', get(option, 'index'), scroll);
+    }
+
+    set(this, '_activateOptionChars', chars);
+    set(this, '_activateOptionMs', ms);
+    set(this, '_activateOptionIndex', index);
+  },
+
+  _findOptionsMatchingChars(chars) {
+    chars = escapeRegExp(chars);
+
+    const pattern = new RegExp(`^${chars}`, 'i');
+
+    return this.options.filter(option => {
+      return pattern.test(collapseWhitespace(option.domElement.textContent));
+    });
+  },
+
+  _activatedOption() {
+    const activeOption = get(this, 'activeOption');
+
+    if (activeOption) {
+      activeOption.send('_activated');
+    }
+  },
+
+  _activatedSelectedOption() {
+    const activeSelectedOption = get(this, 'activeSelectedOption');
+
+    if (activeSelectedOption) {
+      activeSelectedOption.send('_activated');
+    }
+  },
+
+  _deactivateOptions() {
+    set(this, 'activeOptionIndex', -1);
+  },
+
+  _deactivateSelectedOptions() {
+    set(this, 'activeSelectedOptionIndex', -1);
+  },
+
+  _scrollActiveOptionIntoView() {
+    const activeOption = get(this, 'activeOption');
+
+    if (activeOption) {
+      scrollIntoView(activeOption.domElement);
+    }
+  },
+
+  _scrollActiveSelectedOptionIntoView() {
+    const activeSelectedOption = get(this, 'activeSelectedOption');
+
+    if (activeSelectedOption) {
+      scrollIntoView(activeSelectedOption.domElement);
+    }
+  }
 });
--- tests/integration/components/select-box/activating-options-test.js
+++ tests/integration/components/select-box/activating-options-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { later } from '@ember/runloop';
-import { COLLECT_CHARS_MS } from '@zestia/ember-select-box/mixins/select-box/activatable-options';
+import { COLLECT_CHARS_MS } from '@zestia/ember-select-box/components/select-box';
 import {
   render,
   click,
