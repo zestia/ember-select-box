3a7265eebdae0a9a503faefc6e43d9c2613b5d81
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-08-29 09:11:55 +0100

    refactor

--- addon/utils/select-box/api.js
+++ addon/utils/select-box/api.js
@@ -17,26 +17,26 @@ const publicProperties = {
 };
 
 const publicActions = {
-  // 'open': true,
-  // 'close': true,
-  // 'toggle': true,
-  // 'select': true,
-  // 'update': true,
-  // 'selectActiveOption': true,
-  // 'search': true,
-  // 'stopSearching': true,
+  'open': true,
+  'close': true,
+  'toggle': true,
+  'select': true,
+  'update': true,
+  'selectActiveOption': true,
+  'search': true,
+  'cancelSearch': true,
   'setInputValue': true,
   'focusInput': true,
   'blurInput': true,
-  // 'activateOptionAtIndex': true,
-  // 'activateNextOption': true,
-  // 'activatePreviousOption': true,
-  // 'activateOptionForKeyCode': true,
-  // 'deactivateOptions': true,
-  // 'activateSelectedOptionAtIndex': true,
-  // 'activateNextSelectedOption': true,
-  // 'activatePreviousSelectedOption': true,
-  // 'deactivateSelectedOptions' : true
+  'activateOptionAtIndex': true,
+  'activateNextOption': true,
+  'activatePreviousOption': true,
+  'activateOptionForKeyCode': true,
+  'deactivateOptions': true,
+  'activateSelectedOptionAtIndex': true,
+  'activateNextSelectedOption': true,
+  'activatePreviousSelectedOption': true,
+  'deactivateSelectedOptions' : true
 };
 
 export default function api() {
