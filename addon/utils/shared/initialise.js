19d09683bf7820b4972c45df73ad17d11f169a5d
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-08-30 16:05:34 +0100

    refactor

---
+++ addon/utils/shared/initialise.js
@@ -0,0 +1,39 @@
+import invokeAction from '../component/invoke-action';
+import { set } from '@ember/object';
+import { getAPI } from '../component/api';
+
+export function open(selectBox) {
+  if (selectBox.isDestroyed || selectBox.isOpen) {
+    return;
+  }
+
+  set(selectBox, 'isOpen', true);
+
+  opened(selectBox);
+}
+
+export function close(selectBox) {
+  if (selectBox.isDestroyed || !selectBox.isOpen) {
+    return;
+  }
+
+  set(selectBox, 'isOpen', false);
+
+  closed(selectBox);
+}
+
+function opened(selectBox) {
+  invokeAction(selectBox, 'onOpen', getAPI(selectBox));
+}
+
+function closed(selectBox) {
+  invokeAction(selectBox, 'onClose', getAPI(selectBox));
+}
+
+export function toggle(selectBox) {
+  if (selectBox.isOpen) {
+    close(selectBox);
+  } else {
+    open(selectBox);
+  }
+}
