43bdea610911e32e5f503855a9ca80cade660b76
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-02-13 14:01:47 +0000

    run ember-cli-update

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,14 +1,12 @@
 import Application from '@ember/application';
-import Resolver from './resolver';
+import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-const app = Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver
-});
+export default class App extends Application {
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver;
+}
 
-loadInitializers(app, config.modulePrefix);
-
-export default app;
+loadInitializers(App, config.modulePrefix);
