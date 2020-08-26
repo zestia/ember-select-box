de72e0c25da4053cb06a5d84d57b4e9146dc8d6f
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-08-26 11:17:26 +0100

    rename component

--- tests/dummy/app/components/backtrack-select.js
+++ tests/dummy/app/components/backtrack-select.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
-export default class SearchSelectComponent extends Component {
+export default class BacktrackSelectComponent extends Component {
   @tracked lastQuery;
   @tracked results = [];
 
