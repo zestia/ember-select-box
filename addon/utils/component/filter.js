7d086ce31f78ace8424c5ac12589eb43be9580ec
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-09-01 10:46:08 +0100

    simplfy typeahead checks

--- addon/utils/component/filter.js
+++ addon/utils/component/filter.js
@@ -1,12 +1,11 @@
-import escapeRegExp from '../general/escape-regexp';
 import collapseWhitespace from '../general/collapse-whitespace';
 
 export function filterComponentsByTextContent(components, query) {
-  query = escapeRegExp(query);
-
-  const pattern = new RegExp(`^${query}`, 'i');
-
-  return components.filter((component) =>
-    pattern.test(collapseWhitespace(component.element.textContent))
-  );
+  return components.filter((component) => {
+    return (
+      collapseWhitespace(component.element.textContent.toLowerCase()).indexOf(
+        query.toLowerCase()
+      ) !== -1
+    );
+  });
 }
