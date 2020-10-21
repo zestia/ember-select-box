ae244a430d7ad42a4a5bd7c429f0615afac745e6
Phillip Haines <philliphaines@users.noreply.github.com>
2020-10-21 12:37:37 +0100

    update deps and lts test versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,18 +6,18 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
             'ember-source': '~3.16.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.0'
           }
         }
       },
