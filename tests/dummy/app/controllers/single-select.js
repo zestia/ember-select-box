19264a733aa4992f3df3c384a4e1112c82fde48d
Andrew Kirwin <andrew.kirwin@gmail.com>
2020-12-03 22:29:43 +0000

    clean up controller

--- tests/dummy/app/controllers/single-select.js
+++ tests/dummy/app/controllers/single-select.js
@@ -1,29 +1,15 @@
 import Controller from '@ember/controller';
-import { breads, bloomer, brioche } from '../utils/dummy-data';
+import { breads } from '../utils/dummy-data';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class SingleSelectController extends Controller {
   @tracked selectedBread;
-  @tracked performedAction1;
-  @tracked performedAction2;
 
   selectableBreads = breads;
-  bloomer = bloomer;
-  brioche = brioche;
 
   @action
   selectBread(bread) {
     this.selectedBread = bread;
-  }
-
-  @action
-  action1() {
-    this.performedAction1 = true;
-  }
-
-  @action
-  action2() {
-    this.performedAction2 = true;
   }
 }
