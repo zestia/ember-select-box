bb06007294d7496843c99cacf6d84a663f73468f
Camille TJHOA <camille.tjhoa@outlook.com>
2021-03-30 10:07:46 +0000

    Fix FooSelectBox component declaration
    https://github.com/embroider-build/embroider/issues/522

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -17,5 +17,16 @@ module.exports = function (defaults) {
   // This build file does *not* influence how the addon or the app using it
   // behave. You most likely want to be modifying `./index.js` or app's build file
 
-  return maybeEmbroider(app);
+  return maybeEmbroider(app, {
+    packageRules: [
+      {
+        package: 'dummy',
+        components: {
+          '{{foo-select-box}}': {
+            safeToIgnore: true
+          }
+        }
+      }
+    ]
+  });
 };
