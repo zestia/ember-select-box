093d7d05503393613b541a4857650e2f18796c37
Andrew Kirwin <andrew.kirwin@zestia.com>
2021-04-06 09:23:59 +0100

    fix borked release it config

--- .release-it.js
+++ .release-it.js
@@ -1,20 +1,5 @@
-# unconventional js
-/blueprints/*/files/
-/vendor/
-
-# compiled output
-/dist/
-/tmp/
-
-# dependencies
-/bower_components/
-/node_modules/
-
-# misc
-/coverage/
-.eslintcache
-
-# ember-try
-/.node_modules.ember-try/
-/bower.json.ember-try
-/package.json.ember-try
+module.exports = {
+  hooks: {
+    'before:init': ['npm test']
+  }
+};
