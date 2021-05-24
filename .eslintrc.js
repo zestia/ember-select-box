47865f2ff20018bfa8976175bc45a2950857a571
Andrew Kirwin <andrew.kirwin@zestia.com>
2021-05-24 14:13:49 +0100

    upgrade deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 'use strict';
 
-module.exports = require('@zestia/eslint-config/ember-addon');
+module.exports = {
+  extends: '@zestia/eslint-config/ember-addon',
+
+  rules: {
+    // Temporarily disabled due to:
+    // https://github.com/ember-template-lint/eslint-plugin-hbs/issues/42
+    // https://github.com/ember-template-lint/eslint-plugin-hbs/issues/44
+    'hbs/check-hbs-template-literals': 'off'
+  }
+};
