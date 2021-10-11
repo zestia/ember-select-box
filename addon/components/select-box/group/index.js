4cbf7dff7bbc85f6b5715ef9072e940c4d396d99
Andrew Kirwin <andrew.kirwin@gmail.com>
2021-10-11 19:58:12 +0100

    class prop

--- addon/components/select-box/group/index.js
+++ addon/components/select-box/group/index.js
@@ -2,7 +2,5 @@ import Component from '@glimmer/component';
 import { guidFor } from '@ember/object/internals';
 
 export default class SelectBoxGroup extends Component {
-  get labelId() {
-    return guidFor(this);
-  }
+  labelId = guidFor(this);
 }
