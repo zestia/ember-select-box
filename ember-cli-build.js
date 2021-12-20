7b4905c1b144a50531ab9ce360eff37ca6938020
Andrew Kirwin <github@andrewkirwin.co.uk>
2021-12-20 11:09:01 +0000

    correct ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,21 +1,25 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-const { maybeEmbroider } = require('@embroider/test-setup');
-
-process.env.buildTarget = EmberAddon.env();
 
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
-    minifyCSS: {
-      enabled: false
-    }
+    // Add options here
   });
 
-  // This build file specifies the options for the dummy test app of this
-  // addon, located in `/tests/dummy`
-  // This build file does *not* influence how the addon or the app using it
-  // behave. You most likely want to be modifying `./index.js` or app's build file
+  /**
+    This build file specifies the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
 
-  return maybeEmbroider(app);
+  const { maybeEmbroider } = require('@embroider/test-setup');
+  return maybeEmbroider(app, {
+    skipBabel: [
+      {
+        package: 'qunit'
+      }
+    ]
+  });
 };
