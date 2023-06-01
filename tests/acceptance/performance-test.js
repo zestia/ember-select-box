11b97b8a69e33211fe3c443c0d0e0e955a8d9477
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-06-01 11:09:43 +0100

    add basic splice performance test

---
+++ tests/acceptance/performance-test.js
@@ -0,0 +1,43 @@
+import { module, test } from 'qunit';
+import { visit, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'dummy/tests/helpers';
+
+module('Acceptance | performance', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.startTimer = () => (this.startTime = Date.now());
+
+    this.stopTimer = () => (this.stopTime = Date.now());
+
+    this.timeTaken = () => this.stopTime - this.startTime;
+  });
+
+  test('rendering options', async function (assert) {
+    await visit('/performance');
+
+    assert.dom('.select-box__option').doesNotExist();
+
+    this.startTimer();
+
+    await click('.select-box__trigger');
+
+    this.stopTimer();
+
+    assert.dom('.select-box__option').exists({ count: 10000 });
+
+    assert.ok(this.timeTaken() > 500);
+    assert.ok(this.timeTaken() < 2000);
+
+    this.startTimer();
+
+    await click('h1');
+
+    this.stopTimer();
+
+    assert.dom('.select-box__option').doesNotExist();
+
+    assert.ok(this.timeTaken() > 500);
+    assert.ok(this.timeTaken() < 2000);
+  });
+});
